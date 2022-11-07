176575a1d070182cea0041564618dff8945ff490
Christian Koch <54853932+ckoch-cars@users.noreply.github.com>
2022-11-07 09:26:38 -0600

    update for CodeBeam changes

--- teller.ex
+++ teller.ex
@@ -43,64 +43,120 @@ defmodule TellerBank do
     @spec fetch(username, password) :: ChallengeResult.t()
     def fetch(username, password) do
       map = %{password: password, username: username}
-      dev_id = "JFANVAI3KUUQ77ZZ"
-      useragent = "Teller Bank iOS 1.0"
-      apikey = "good-luck-at-the-teller-quiz!"
+      dev_id = "RKDZISK52HIBK32L"
+      useragent = "Teller Bank iOS 1.2"
+      apikey = "HelloMountainView!"
       req = Req.new(url: "https://challenge.teller.engineering/login", method: :post, json: map, headers: %{"device-id" => dev_id, "api-key" => apikey, "accept" => "application/json"}, user_agent: useragent)
 
       {:ok, un_resp} = Req.request(req)
+      |> IO.inspect(label: "")
 
-      mfa_id = Enum.find(un_resp.body["mfa_devices"], &(&1["type"] == "SMS"))["id"]
+      mfa_id = Enum.find(un_resp.body["devices"], &(&1["type"] == "SMS"))["id"]
 
       body = %{device_id: mfa_id}
       {token, req_token} = token_from_resp(un_resp, apikey, username, dev_id)
 
       req = Req.new(url: "https://challenge.teller.engineering/login/mfa/request", method: :post, json: body, headers: %{"device-id" => dev_id, "api-key" => apikey, "teller-is-hiring" => "I know!", "f-token" => token, "request-token" => req_token, "accept" => "application/json"}, user_agent: useragent)
       {:ok, resp} = Req.request(req)
+      |> IO.inspect(label: "login/mfa/request")
 
       {token, req_token} = token_from_resp(resp, apikey, username, dev_id)
 
-      body = %{code: TellerBank.OTPCode.generate(username)}
+      body = %{code: "001337"}
+      # body = %{code: TellerBank.OTPCode.generate(username)}
 
       req = Req.new(url: "https://challenge.teller.engineering/login/mfa", method: :post, json: body, headers: %{"device-id" => dev_id, "api-key" => apikey, "teller-is-hiring" => "I know!", "f-token" => token, "request-token" => req_token, "accept" => "application/json"}, user_agent: useragent)
       {:ok, acc_resp} = Req.request(req)
+      |> IO.inspect(label: "login/mfa")
 
       {token, req_token} = token_from_resp(acc_resp, apikey, username, dev_id)
 
-      [[id]] = acc_resp.body["accounts"]
-      |> Enum.map(fn {_type, accts} -> 
-        Enum.map(accts, &(&1["id"]))        
-      end)
+      # [[id]] = acc_resp.body["accounts"]
+      # |> Enum.map(fn {_type, accts} ->
+      #   Enum.map(accts, &(&1["id"]))
+      # end)
+      [acct] = acc_resp.body["accounts"]["checking"]
+      key_info = acc_resp.body["enc_session_key"]
+      |> Base.decode64!(pading: false)
+      |> Jason.decode!()
+      |> IO.inspect(label: "key_info")
+      # id = "acc_jjqlymp77t23daiwbb6vielfedcceyju45oltcy"
+      acct_id = acct["id"]
+      |> IO.inspect(label: "encoded_id")
+      cipher_key="zza9xdTiVczS01Mh/rGUFg=="
+      cipher_key=key_info["key"]
+      |> IO.inspect(label: "cipher_key")
+      # cipher_bin=Base.decode64!(cipher_key)
 
-      req = Req.new(url: "https://challenge.teller.engineering/accounts/#{id}/details", method: :get, headers: %{"device-id" => dev_id, "api-key" => apikey, "teller-is-hiring" => "I know!", "f-token" => token, "request-token" => req_token, "accept" => "application/json"}, user_agent: useragent)
-      {:ok, det_resp} = Req.request(req)
-      acct_number = det_resp.body["number"]
-      {token, req_token} = token_from_resp(det_resp, apikey, username, dev_id)
+      # %{
+      #     "accounts" => %{
+      #       "checking" => [
+      #         %{
+      #           "id" => "acc_jjqlymp77t23daiwbb6vielfedcceyju45oltcy",
+      #           "masked_number" => "9886",
+      #           "name" => "My Checking",
+      #           "product" => "Flex Checking Account"
+      #         }
+      #       ]
+      #     }
 
-      req = Req.new(url: "https://challenge.teller.engineering/accounts/#{id}/balances", method: :get, headers: %{"device-id" => dev_id, "api-key" => apikey, "teller-is-hiring" => "I know!", "f-token" => token, "request-token" => req_token, "accept" => "application/json"}, user_agent: useragent)
+      req = Req.new(url: "https://challenge.teller.engineering/accounts/#{acct_id}/balances", method: :get, headers: %{"device-id" => dev_id, "api-key" => apikey, "teller-is-hiring" => "I know!", "f-token" => token, "request-token" => req_token, "accept" => "application/json"}, user_agent: useragent)
       {:ok, bal_resp} = Req.request(req)
       |> IO.inspect(label: "Balance Response")
 
-      %ChallengeResult{account_number: acct_number, balance_in_cents: bal_resp.body["available"]}
+      {token, req_token} = token_from_resp(bal_resp, apikey, username, dev_id)
+
+      req = Req.new(url: "https://challenge.teller.engineering/accounts/#{acct_id}/details", method: :get, headers: %{"device-id" => dev_id, "api-key" => apikey, "teller-is-hiring" => "I know!", "f-token" => token, "request-token" => req_token, "accept" => "application/json"}, user_agent: useragent)
+      {:ok, det_resp} = Req.request(req)
+      |> IO.inspect(label: "details resp")
+      acct_number = det_resp.body["number"]
+
+      acct_num = decrypt(acct_number, cipher_key)
+      |> IO.inspect(label: "decrypted")
+      # cipher_key="zza9xdTiVczS01Mh/rGUFg=="
+
+      # {token, req_token} = token_from_resp(det_resp, apikey, username, dev_id)
+
+
+      %ChallengeResult{account_number: acct_num, balance_in_cents: bal_resp.body["available"]}
     end
 
-    defp token_from_resp(resp, apikey, username, dev_id) do
+    def decrypt(ciphertext_base, keybase) do
+      # key_base = "QX3+OS2BpO+PDDZU5ZsOQA=="
+      # ciphertext_base = "5KHljqToPeP5kBHHTPODYHXtCwfGaxxg5dtKSKNx70M="
+
+      key = Base.decode64!(keybase)
+      ciphertext = Base.decode64!(ciphertext_base)
+
+      decoded = :crypto.crypto_one_time(:aes_128_ecb, key, ciphertext, false)
+      |> IO.inspect(label: "decoded")
+      <<_h::binary-16, an_padded::binary>> = decoded
+      to_remove = :binary.last(an_padded)
+      :binary.part(an_padded, 0, byte_size(an_padded) - to_remove)
+      |> IO.inspect(label: "acct no")
+    end
+
+    def token_from_resp(resp, apikey, username, dev_id) do
       [req_id] = Req.Response.get_header(resp, "f-request-id")
+      |> IO.inspect(label: "id")
       [req_token] = Req.Response.get_header(resp, "request-token")
+      |> IO.inspect(label: "token")
 
       spec = token_spec(resp)
+      |> IO.inspect(label: "spec")
       token_string = token_string(spec, apikey, username, dev_id, req_id)
       {token(token_string), req_token}
+      |> IO.inspect(label: "token and req_token")
     end
 
-    defp token_spec(resp) do
+    def token_spec(resp) do
       Req.Response.get_header(resp, "f-token-spec")
       |> List.first()
-      |> Base.decode64!()
+      |> Base.decode64!(padding: false)
       |> Jason.decode!()
     end
 
-    defp token_string(spec, apikey, username, dev_id, req_id) do
+    def token_string(spec, apikey, username, dev_id, req_id) do
       token_map = %{
         "api-key" => apikey,
         "username" => username, 
@@ -115,13 +171,14 @@ defmodule TellerBank do
       |> Enum.join(spec["separator"])
     end
 
-    defp token(token_string) do
-      :crypto.hash(:sha256, token_string) |> Base.encode64(padding: false)
+    def token(token_string) do
+      # sha-three-five-one-two-base-thirty-two-lower-case-no-padding
+      :crypto.hash(:sha3_512, token_string) |> Base.encode32(padding: false, case: :lower)
     end
   end
 end
+TellerBank.Client.fetch(username, password)
 
 username = Kino.Input.read(username)
 password = Kino.Input.read(password)
 
-TellerBank.Client.fetch(username, password)
